79406405966329d7a9dbaefe9379f74b2fbd1eab
Jaremy Longley <jlongley0314@gmail.com>
2019-05-29 22:10:12 -0500

    starting the main receipts page

--- receipts-genius/server.js
+++ receipts-genius/server.js
@@ -6,15 +6,9 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/api/greeting', (req, res) => {
-    const name = req.query.name || 'World';
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
-
 app.get('/api/receipts', (req, res) => {
     res.sendFile(path.join(__dirname, './assets', 'receipt_data.json'));
-})
+});
 
 app.listen(3001, () =>
     console.log('Express server is running on localhost:3001')
--- receipts-genius/src/components/receipts.js
+++ receipts-genius/src/components/receipts.js
@@ -2,17 +2,25 @@ import React from 'react'
 
 const Receipts = ({ receipts }) => {
     return (
-        <div>
-            <center><h1>Receipt List</h1></center>
-            {receipts.map((receipt) => (
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{receipt.name}</h5>
-                        {/*<h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>*/}
-                        {/*<p class="card-text">{contact.company.catchPhrase}</p>*/}
-                    </div>
+        <div class="App-main">
+            <div class="App-header">
+                <center><h3><em>Receipt</em>Genius</h3></center>
+            </div>
+
+            <div class="App-body">
+                <div class="sub-header">
+
                 </div>
-            ))}
+                <div class="receipts-container">
+                {receipts.map((receipt) => (
+                    <div class="single-receipt-container">
+                        <div class="receipt-body">
+                            <h5 class="card-title">{receipt.name}</h5>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </div>
         </div>
     )
 };
